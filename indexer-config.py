7c502d0bd39b9a41091b64d69c215f5f676c663b
AWSCloudEng <79238784+ilabunix@users.noreply.github.com>
2026-01-21 13:24:19 -0600

    Create indexer-config.py

---
+++ indexer-config.py
@@ -0,0 +1,13 @@
+# config.py
+
+CONFLUENCE_URL = "https://irshad-mohammed.atlassian.net/wiki/"
+CONFLUENCE_EMAIL = "irshad-mohamed@outlook.com"
+CONFLUENCE_API_TOKEN = "ATATT3xFf6G0wp9k6ZoXu3OGaHKERBTKB1BgzkhZnbxdkSByoR3JJ-DQ530RPQlkLK_nwBq-OpqgByvk9RCBX"  # Rotate this in prod
+CONFLUENCE_SPACE_KEY = "CloudOpsRu"
+
+OPENSEARCH_ENDPOINT = "https://search-cloudops-chatbot-j5k467hw6lnuikdfsumuue3jppi.us-west-2.es.amazonaws.com"
+OPENSEARCH_INDEX = "confluence-pages"
+
+# Basic auth for OpenSearch
+OS_USERNAME = "admin"
+OS_PASSWORD = "Amazon12!@"
